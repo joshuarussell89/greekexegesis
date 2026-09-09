1b7aca6f7471db099d9f5149f0333bab01534bf2
joshuarussell89 <rchjrhbmf8@privaterelay.appleid.com>
2026-09-09 15:41:17 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    having instantly.
 
    Books and diagrams live in IndexedDB and are never touched here. */
-const VERSION = "2026-08-28e";
+const VERSION = "2026-09-05a";
 const CACHE   = "nous-" + VERSION;
 const SHELL = [
   "./",
